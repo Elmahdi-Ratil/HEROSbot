36cb462582f7b45e8dc86aa6a4bef20e2c6f3186
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-29 18:34:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '!'
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`Hey, Wassup!`);
+
+
+
+
+client.on('ready', function() {
+	console.log(`i am ready ${client.user.username}`);
+    client.user.setGame('Mido King');
 });
+client.on('voiceStateUpdate', (o, n) => {
+        let newUserChannel = n.voiceChannel
+    let oldUserChannel = o.voiceChannel
 
-bot.on("message", async message => {
+    var channel = client.channels.get("اي دي الروم الي يرسل فيه");
+        let cha = n.guild.channels.get("اي دي الروم الصوتي الي يستجيب منه");
+        
+          let mute1 = o.serverMute;
+  let mute2 = n.serverMute;
+  
 
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
+  let deafen1 = o.serverDeaf;
+  let deafen2 = n.serverDeaf;
 
-  let prefix = '-';
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+    if(mute1 === false && mute2 === true) return;
+    if(mute1 === true && mute2 === false) return;
+    if(deafen1 === false && deafen2 === true) return;
+    if(deafen1 === true && deafen2 === false) return;
+    
 
-  if (cmd === `${prefix}ping`){
-    message.channel.send("Pong!");
-  }
-});
+    channel.send(`Join Room ${n.displayName}`)
 
+})
 
 bot.login(process.env.BOT_TOKEN);
