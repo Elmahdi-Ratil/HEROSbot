964e91b19a7dc86e452c1ddc61842708c50a4065
heros4 <40445911+heros4@users.noreply.github.com>
2018-07-26 22:25:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const client = new Discord.Client();
+const prefix = '+'
+client.on('ready', () => {
+  console.log('╔[════════════════════════════════════]╗');
+  console.log('')
+  console.log('            ╔[════════════]╗')
+  console.log('              Bot Is Online')
+  console.log('            ╚[════════════]╝')
+  console.log('')
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('')
+  console.log('╚[════════════════════════════════════]╝')
+});
+client.on('ready', () => {
+     client.user.setActivity("bot",{type: 'WATCHING'})
 
+});
+
+   
 client.on('message', message => {
-    if(message.content.includes('discord.gg')){ 
+    if(message.content.includes('discord.gg')){     
                                             if(!message.channel.guild) return message.reply('');
         if (!message.member.hasPermissions(['ADMINISTRATOR'])){
         message.delete()
@@ -11,8 +32,16 @@ client.on('message', message => {
     }
 }
 });
-   
 
+client.on('message', message => {
+    if(message.content.includes('youtube.com')){     
+                                            if(!message.channel.guild) return message.reply('');
+        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+        message.delete()
+    return message.reply(`** يمنع نشر الروابط في هاذا السيرفر:angry: ! **`)
+    }
+}
+});
 
 
 اللحية.login(process.env.BOT_TOKEN);
