0cacd20ee47678232e90f8288f4e242fd97b8c27
mahdi <40445911+heros4@users.noreply.github.com>
2019-04-23 19:59:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,77 +2,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const client = new Discord.Client();
-const prefix = '+'
-client.on('ready', () => {
-  console.log('╔[════════════════════════════════════]╗');
-  console.log('')
-  console.log('            ╔[════════════]╗')
-  console.log('              Bot Is Online')
-  console.log('            ╚[════════════]╝')
-  console.log('')
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log('')
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log('')
-  console.log('╚[════════════════════════════════════]╝')
-});
-client.on('ready', () => {
-     client.user.setActivity("bot",{type: 'WATCHING'})
 
-});
-
-client.on('message', message => {
-    if(message.content.includes('discord.gg')){     
-                                            if(!message.channel.guild) return message.reply('');
-        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-        message.delete()
-    return message.reply(`** يمنع نشر الروابط في هاذا السيرفر:angry: ! **`)
-    }
-}
-});
-
-client.on('message', message => {
-    if(message.content.includes('youtube.com')){     
-                                            if(!message.channel.guild) return message.reply('');
-        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-        message.delete()
-    return message.reply(`** يمنع نشر الروابط في هاذا السيرفر:angry: ! **`)
-    }
-}
-});
-
-client.on('message', message => {
-    if (message.content.startsWith("server")) {
-
-  message.channel.createInvite({
-        thing: true,
-        maxUses: 100,
-        maxAge: 86400
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-                .setAuthor(message.guild.name, message.guild.iconURL)
-        .setDescription(`
-**
----------------------
--[${message.guild.name}]  هذا هو رابط سيرفر
----------------------
--هذا الرابط صالح ل 100 مستخدم فقط
----------------------
--هذا الرابط صالح لمده 24 ساعه فقط
----------------------
-**`)
-      message.author.sendEmbed(Embed11)
-    }
-});
 
 
 client.on('message', async message => {
